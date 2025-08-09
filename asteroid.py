c052449737ed450ff1c0812b16789aada6ca9fac
deniscales <dennis.scales@gmail.com>
2025-08-09 12:13:06 -0400

    defined asteroid

---
+++ asteroid.py
@@ -0,0 +1,13 @@
+from circleshape import CircleShape
+import pygame
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.color = (255, 255, 255)  # Default color white
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (int(self.position.x), int(self.position.y)), self.radius, width=2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
