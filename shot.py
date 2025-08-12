fbe3ede62c5413b0028f6a89a7d10d72c4455d21
deniscales <dennis.scales@gmail.com>
2025-08-11 20:18:53 -0400

    added for shooting

---
+++ shot.py
@@ -0,0 +1,15 @@
+from circleshape import CircleShape
+import pygame
+from constants import *
+
+class Shot(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.color = (255, 0, 0)  # Default color red
+        self.velocity = pygame.Vector2(0, -1)  # Initial velocity upwards
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (int(self.position.x), int(self.position.y)), self.radius)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
